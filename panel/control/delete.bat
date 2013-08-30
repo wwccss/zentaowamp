329019b7b367a655062d8946739ae5cb36d09a6d
iamazhi <chencongzhi520@gmail.com>
2013-08-30 14:25:34 +0800

    * corrent the directory.

---
+++ panel/control/delete.bat
@@ -0,0 +1,12 @@
+del build.bat
+del build-zt.bat
+del build-xr.bat
+del glue.exe
+del null
+del wsrlua.exe
+rd /S /Q config
+rd /S /Q lang 
+del /Q tmp
+del /Q log
+cd ../../
+
---
+++ panel/control/lang/en.lua
@@ -0,0 +1,136 @@
+lang = {}
+lang.title  = {}
+lang.button = {}
+lang.prompt = {}
+lang.menu = {}
+lang.url = {}
+
+-------------------------------------
+-- Url
+lang.url.officialSite   = "http://www.zentao.net/en/"
+lang.url.proHelp        = ""
+lang.url.opensourceHelp = ""
+lang.url.flowChart      = ""
+lang.url.faq            = ""
+lang.url.forum          = ""
+
+-------------------------------------
+-- Menu.
+-------------------------------------
+lang.title.service         = 'Services(S)'
+lang.menu.viewService      = "View Services(V)"
+lang.menu.uninstallService = "Uninstall Services(U)"
+
+lang.title.more        = 'More(M)'
+lang.menu.initBat      = ""
+lang.menu.database     = ""
+lang.menu.backup       = ""
+lang.menu.checkVersion = ""
+lang.title.lang        = 'Language(L)'
+lang.menu.zhcn         = 'ÖÐÎÄ(Z)'
+lang.menu.en           = 'English(E)'
+
+lang.title.help        = ''
+lang.menu.officialSite = ""
+lang.menu.officialHelp = ''
+lang.menu.flowChart    = ""
+lang.menu.faq          = ""
+lang.menu.forum        = ""
+
+lang.menu.exit         = 'Exit(X)'
+-------------------------------------
+-- Buttons.
+-------------------------------------
+lang.button.start        = "Start"
+lang.button.running      = 'Running...'
+lang.button.access       = "Access"
+lang.button.stop         = "Stop"
+lang.button.stopped      = 'stoped'
+lang.button.officialSite = 'zentao.net'
+
+lang.button.ok      = 'Ok'
+lang.button.cancel  = 'cancel'
+lang.button.save    = 'Save'
+lang.button.install = 'Install'
+
+-------------------------------------
+-- Dialog title.
+-------------------------------------
+lang.title.controlPanel       = 'ZenTao Integrated Run Environment'
+lang.title.configPort         = 'Port configure'
+lang.title.alarm              = 'Prompt'
+lang.title.warning            = 'Warning'
+lang.title.exit               = 'Exit'
+lang.title.backupSuccessfully = 'Backup successfully'
+
+-------------------------------------
+-- Prompt.
+-------------------------------------
+lang.prompt.newLine = "\n"
+
+lang.prompt.fileUnfindable = "Can't find %s"
+
+-- Install VC.
+lang.prompt.withoutVC2005Redistributable = "Need 'VC++ 2005 redistributable'£¬Please install first"
+lang.prompt.withoutVC2008Redistributable = "Need 'VC++ 2008 redistributable'£¬Please install first"
+
+
+-- Control panel version and current dir.
+lang.prompt.currentDir = 'Current Directory:%s'
+lang.prompt.panel      = 'ZenTao Control Panel Version %s'
+lang.prompt.trayTip    = lang.prompt.version
+
+-- Xampp is not under root directory.
+lang.prompt.wrongPath = "Please extract the xampp to root directory, example:D:\\xampp"
+
+-- Control Panel is running.
+lang.prompt.panelIsRunning = 'Control panel is running'
+
+-- Uninstall service.
+lang.prompt.tryToUninstallService = 'Try to uninstall %s service......'
+lang.prompt.uninstallSuccessfully = 'Uninstall %s service successfully'
+lang.prompt.uninstallFailed       = 'Uninstall %s service failed'
+lang.prompt.serviceUnfindable     = "%s service is uninstalled"
+
+-- Init bat.
+lang.prompt.initBatSuccessfully = 'Init bat successfully! enter %s?'
+lang.prompt.initBatFailed       = 'Init failed!'
+
+-- Backup data.
+lang.prompt.mysqlNotRunning   = 'Mysql is stopped, so just backup data filem.'
+lang.prompt.backuping         = 'Backuping...'
+lang.prompt.backupAlarm       = "Success to backup to %s directory, enter this directory?"
+
+-- Check new version.
+lang.button.getNewVersion    = 'Get new version.'
+lang.prompt.cannotGetUpdater = 'Failed to connect service, wrong network or the version in %s is incorrect.'
+lang.prompt.isLatest         = 'Your current version of pms is newest.'
+
+-- Start ZenTao.
+lang.prompt.tryToStartServices   = 'Try to start services......'
+lang.prompt.tryToInstallService  = 'Try to install %s service......'
+lang.prompt.installSuccessfully  = 'Install %s service successfully.'
+lang.prompt.serviceIsRunning     = '%s service is running, port:%d'
+lang.prompt.startSuccessfully    = 'Servieces is running, click "access" button to access.'
+lang.prompt.startFailed          = 'Start %s failed'
+lang.prompt.installFailed        = 'Install %s service failed'
+lang.prompt.tryToStart           = 'Try to start......'
+lang.prompt.unknownServiceStatus = 'Unknow service status'
+lang.prompt.serviceExists        = '%s service exists'
+
+-- Stop ZenTao.
+lang.prompt.tryToStopServices       = 'Try to stop services......'
+lang.prompt.stopServiceSuccessfully = 'stop %s service successfully'
+lang.prompt.stopServiceFailed       = 'stop %s service failed'
+lang.prompt.stopSuccessfully        = 'Services is stopped, click "start" button to restart.'
+lang.prompt.serviceIsStopped        = '%s service is stopped'
+
+-- Port is conflict.
+lang.prompt.portIsConflict = 'The port of %s%d is conflict'
+lang.prompt.suggestPort    = '£¬suggested port:'
+
+lang.version = {}
+lang.version.php        = 'php Version£º%s'
+lang.version.apache     = 'apache Version£º%s'
+lang.version.mysql      = 'mysql Version£º%s'
+lang.version.phpmyadmin = 'phpmyadmin Version£º%s'
