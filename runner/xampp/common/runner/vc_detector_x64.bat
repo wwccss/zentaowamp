5a5f8f0a25781dc2d0f8264f475796a06eab6383
Catouse <catouse@gmail.com>
2017-06-12 16:59:14 +0800

    * change vc detector bat file.

--- runner/xampp/common/runner/vc_detector_x64.bat
+++ runner/xampp/common/runner/vc_detector_x64.bat
@@ -1,7 +1,9 @@
 @echo off
 set "HKLMU=HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Dependencies"
 ::::: 判断系统是否已经安装了 Visual C++ 2015 
-reg query %HKLMU%\{BC958BD2-5DAC-3862-BB1A-C1BE0790438D}>nul 2>nul&&set VC2015=Microsoft Visual C++ 2015 x64 Additional Runtime
-reg query %HKLMU%\{0D3E9E15-DE7A-300B-96F1-B4AF12B96488}>nul 2>nul&&set VC2015=Microsoft Visual C++ 2015 x64 Minimum Runtime
-reg query %HKLMU%\{e46eca4f-393b-40df-9f49-076faf788d83}>nul 2>nul&&set VC2015=Microsoft Visual C++ 2015 Redistributable (x64)
-if defined VC2015 (echo INSTALLED: %VC2015% &ping -n 3 127.1>nul)
+reg query %HKLMU%\{BC958BD2-5DAC-3862-BB1A-C1BE0790438D}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x64 Additional Runtime
+reg query %HKLMU%\{0D3E9E15-DE7A-300B-96F1-B4AF12B96488}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x64 Minimum Runtime
+reg query %HKLMU%\{e46eca4f-393b-40df-9f49-076faf788d83}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable x64
+reg query %HKLMU%\{f1e7e313-06df-4c56-96a9-99fdfd149c51}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 Redistributable x64
+reg query "%HKLMU%\,,amd64,14.0,bundle">nul 2>nul&&set VC=Microsoft Visual C++ 2017 Redistributable x64
+if defined VC (echo INSTALLED: %VC%)
--- runner/xampp/common/runner/vc_detector_x86.bat
+++ runner/xampp/common/runner/vc_detector_x86.bat
@@ -1,6 +1,17 @@
 @echo off
 set "HKLMU=HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Dependencies"
 ::::: 判断系统是否已经安装了 Visual C++ 2015 
-reg query %HKLMU%\{A2563E55-3BEC-3828-8D67-E5E8B9E8B675}>nul 2>nul&&set VC2015=Microsoft Visual C++ 2015 x86 Minimum Runtime
-reg query %HKLMU%\{74d0e5db-b326-4dae-a6b2-445b9de1836e}>nul 2>nul&&set VC2015=Microsoft Visual C++ 2015 Redistributable X86
-if defined VC2015 (echo INSTALLED: %VC2015% &ping -n 3 127.1>nul)
+reg query %HKLMU%\{A2563E55-3BEC-3828-8D67-E5E8B9E8B675}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Minimum Runtime
+reg query %HKLMU%\{74d0e5db-b326-4dae-a6b2-445b9de1836e}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable X86
+reg query %HKLMU%\{b64ca997-b626-4abb-a046-5ca2d92ed659}>nul 2>nul&&set VC=Microsoft Visual C++ 2012 Redistributable X86
+reg query %HKLMU%\{f65db027-aff3-4070-886a-0d87064aabb1}>nul 2>nul&&set VC=Microsoft Visual C++ 2013 Redistributable X86
+reg query %HKLMU%\{FF66E9F6-83E7-3A3E-AF14-8DE9A809A6A4}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 Redistributable X86
+reg query %HKLMU%\{9A25302D-30C0-39D9-BD6F-21E6EC160475}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 SP1 Redistributable X86
+reg query %HKLMU%\{1F1C2DFC-2D24-3E06-BCB8-725134ADF989}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 SP1 Redistributable ATL Security Update X86
+reg query %HKLMU%\{350AA351-21FA-3270-8B7A-835434E766AD}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 Redistributable X64
+reg query %HKLMU%\{8220EEFE-38CD-377E-8595-13398D740ACE}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 SP1 Redistributable X64
+reg query %HKLMU%\{4B6C7001-C7D6-3710-913E-5BC23FCE91E6}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 SP1 Redistributable ATL Security Update X64
+reg query %HKLMU%\{2B547B43-DB50-3139-9EBE-37D419E0F5FA}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 Redistributable IA64
+reg query %HKLMU%\{5827ECE1-AEB0-328E-B813-6FC68622C1F9}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 SP1 Redistributable IA64
+reg query %HKLMU%\{977AD349-C2A8-39DD-9273-285C08987C7B}>nul 2>nul&&set VC=Microsoft Visual C++ 2008 SP1 Redistributable ATL Security Update IA64
+if defined VC (echo INSTALLED: %VC%)
