9bbf5a267395abcab973e992b842e2e00a21dbca
Catouse <catouse@gmail.com>
2017-12-21 19:05:09 +0800

    * update vc detector bat files.

--- runner/xampp/common/runner/vc_detector_x86.bat
+++ runner/xampp/common/runner/vc_detector_x86.bat
@@ -1,7 +1,23 @@
 @echo off
-set "HKLMU=HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Dependencies"
+set "HKLMU=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 ::::: 判断系统是否已经安装了 Visual C++ 2015 
 :: { Visual C++ 2015 }
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{58b3beca-b999-4f6f-a48c-81681136a620}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 Redistributable (x86) - 14.10.25017
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{582EA838-9199-3518-A05C-DB09462F68EC}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 x86 Minimum Runtime - 14.10.25017
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{68306422-7C57-373F-8860-D26CE4BA2A15}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 x86 Additional Runtime - 14.10.25017
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{e2803110-78b3-4664-a479-3611a381656a}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable (x86) - 14.0.24215
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{BBF2AC74-720C-3CB3-8291-5E34039232FA}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Minimum Runtime - 14.0.24215
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{69BCE4AC-9572-3271-A2FB-9423BDA36A43}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Additional Runtime - 14.0.24215
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{462f63a8-6347-4894-a1b3-dbfe3a4c981d}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable (x86) - 14.0.24212
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{37B55901-995A-3650-80B1-BBFD047E2911}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Minimum Runtime - 14.0.24212
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{844ECB74-9B63-3D5C-958C-30BD23F19EE4}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Additional Runtime - 14.0.24212
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{206898cc-4b41-4d98-ac28-9f9ae57f91fe}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable (x86) - 14.0.24123
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{06AE3BCC-7612-39D3-9F3B-B6601D877D02}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Minimum Runtime - 14.0.24123
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{03AC7A79-F8AF-38FC-9DA0-98DAB4F4B1CD}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Additional Runtime - 14.0.24123
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{74d0e5db-b326-4dae-a6b2-445b9de1836e}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable (x86) - 14.0.23026
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{A2563E55-3BEC-3828-8D67-E5E8B9E8B675}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Minimum Runtime - 14.0.23026
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{BE960C1C-7BAD-3DE6-8B1A-2616FE532845}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Additional Runtime - 14.0.23026
+
 reg query %HKLMU%\{58b3beca-b999-4f6f-a48c-81681136a620}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 Redistributable (x86) - 14.10.25017
 reg query %HKLMU%\{582EA838-9199-3518-A05C-DB09462F68EC}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 x86 Minimum Runtime - 14.10.25017
 reg query %HKLMU%\{68306422-7C57-373F-8860-D26CE4BA2A15}>nul 2>nul&&set VC=Microsoft Visual C++ 2017 x86 Additional Runtime - 14.10.25017
@@ -17,6 +33,7 @@ reg query %HKLMU%\{03AC7A79-F8AF-38FC-9DA0-98DAB4F4B1CD}>nul 2>nul&&set VC=Micro
 reg query %HKLMU%\{74d0e5db-b326-4dae-a6b2-445b9de1836e}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 Redistributable (x86) - 14.0.23026
 reg query %HKLMU%\{A2563E55-3BEC-3828-8D67-E5E8B9E8B675}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Minimum Runtime - 14.0.23026
 reg query %HKLMU%\{BE960C1C-7BAD-3DE6-8B1A-2616FE532845}>nul 2>nul&&set VC=Microsoft Visual C++ 2015 x86 Additional Runtime - 14.0.23026
+
 :: { Old Visual C++ }
 reg query %HKLMU%\{b64ca997-b626-4abb-a046-5ca2d92ed659}>nul 2>nul&&set VC=Microsoft Visual C++ 2012 Redistributable X86
 reg query %HKLMU%\{f65db027-aff3-4070-886a-0d87064aabb1}>nul 2>nul&&set VC=Microsoft Visual C++ 2013 Redistributable X86
