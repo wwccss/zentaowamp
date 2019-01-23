6602a430ae56493037ab6c8181fdb3feeaa93440
Catouse <catouse@gmail.com>
2019-01-23 20:28:36 +0800

    * change build script.

--- runner/build/index.js
+++ runner/build/index.js
@@ -2,6 +2,7 @@ const fs = require('fs-extra');
 const winresourcer = require('winresourcer');
 const path = require('path');
 const {execFile} = require('child_process');
+const program = require('commander');
 
 const WAMP_PATH = path.resolve(__dirname, '../xampp');
 const APP_PATH = path.resolve(__dirname, '../');
@@ -9,19 +10,30 @@ const APP_PATH = path.resolve(__dirname, '../');
 console.log('APP_PATH', APP_PATH);
 console.log('WAMP_PATH', WAMP_PATH);
 
-let targets = process.argv[2] || '*';
+program
+    .version('1.0.0')
+    .alias('node ./index.js')
+    .option('-t, --targets <targets>', 'like zentao, xuanxuan, all, chanzhi, ranzhi or *', '*')
+    .option('-a, --arch <arch>', 'like win32, win64 or *', '*')
+    .option('-f, --fast', false)
+    .option('-e, --example', false)
+    .parse(process.argv);
+
+let targets = program.targets || '*';
 if (!targets || targets === '*') {
     targets = ['zentao', 'xuanxuan', 'all', 'chanzhi', 'ranzhi'];
 } else {
     targets = targets.split(',');
 }
-let platforms = process.argv[3] || '*';
+let platforms = program.arch || '*';
 if (!platforms || platforms === '*') {
     platforms = ['win32', 'win64'];
 } else {
     platforms = platforms.split(',');
 }
-const isFastMode = !!process.argv[4];
+const isFastMode = program.fast;
+const includeExample = program.example;
+const examplePath = includeExample && fs.pathExistsSync(path.join(WAMP_PATH, 'example-app')) ? path.join(WAMP_PATH, 'example-app') : null;
 
 console.log('Targets', targets);
 console.log('Platforms', platforms);
@@ -58,7 +70,7 @@ const buildTarget = (target, platform = 'win64') => {
     console.log(`Build target: ${target} - ${platform}...`);
     return new Promise((resolve, reject) => {
         const isWin32 = platform === 'win32';
-        const targetPath = path.join(WAMP_PATH, isWin32 ? `wamp_${target}_win32` : `wamp_${target}`);
+        const targetPath = path.join(APP_PATH, 'release', isWin32 ? `wamp_${target}_win32` : `wamp_${target}`);
         const targetExeFile = path.join(targetPath, 'start.exe');
 
         if (!isFastMode) {
@@ -87,7 +99,26 @@ const buildTarget = (target, platform = 'win64') => {
         fs.copySync(path.join(APP_PATH, `start-${platform}.exe`), targetExeFile);
 
         console.log(`\tReplace exe icon`);
-        return replaceIcon(targetExeFile, path.join(targetPath, 'runner', 'icon.ico')).then(resolve).catch(reject);
+        replaceIcon(targetExeFile, path.join(targetPath, 'runner', 'icon.ico')).then(() => {
+            if (includeExample) {
+                const exampleDirPath = path.join(APP_PATH, 'release/examples', isWin32 ? `wamp_${target}_win32_example` : `wamp_${target}_example`);
+                if (!isFastMode) {
+                    fs.emptyDirSync(exampleDirPath);
+                }
+                fs.copySync(targetPath, exampleDirPath);
+                if (examplePath) {
+                    const exampleAppName = target === 'xuanxuan' ? 'xxb' : target;
+                    fs.copySync(examplePath, path.join(exampleDirPath, exampleAppName));
+                }
+                if (target === 'zentao') {
+                    fs.copySync(examplePath, path.join(exampleDirPath, 'zentaoep'));
+                    fs.copySync(examplePath, path.join(exampleDirPath, 'zentaopro'));
+                } else if (target === 'xuanxuan' && fs.pathExistsSync(path.join(WAMP_PATH, 'xxd'))) {
+                    fs.copySync(path.join(WAMP_PATH, 'xxd'), path.join(exampleDirPath, 'xxd'));
+                }
+            }
+            resolve();
+        }).catch(reject);
     });
 };
 
